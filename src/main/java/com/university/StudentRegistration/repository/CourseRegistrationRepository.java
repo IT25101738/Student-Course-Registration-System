4cb0c75be4055d68cb3fe878d0a94cb6b4d721d5
Premakeerthi P.K.A.I <it25103227@my.sliit.lk>
2026-04-18 09:37:52 +0530

    Implemented CourseRegistrationRepository

---
+++ src/main/java/com/university/StudentRegistration/repository/CourseRegistrationRepository.java
@@ -0,0 +1,10 @@
+package com.university.StudentRegistration.repository;
+
+import com.university.StudentRegistration.model.CourseRegistration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRegistrationRepository extends JpaRepository<CourseRegistration, Long> {
+
+}
