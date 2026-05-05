eb55fff50bc64c5f76de883fab42aeb2d22b700b
IT25100697 <it25100697@my.sliit.lk>
2026-05-05 22:00:34 +0530

    Add Enrollment component files

---
+++ src/main/java/com/university/StudentRegistration/repository/EnrollmentRepository.java
@@ -0,0 +1,9 @@
+package com.university.StudentRegistration.repository;
+
+import com.university.StudentRegistration.model.Enrollment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+}
