039033a754a18da780988745b9a18ce2e1bd27cf
vinuga <it25101738@my.sliit.lk>
2026-05-16 18:49:00 +0530

    Updated code with comments

--- src/main/java/com/university/StudentRegistration/model/GraduateStudent.java
+++ src/main/java/com/university/StudentRegistration/model/GraduateStudent.java
@@ -1,22 +1,28 @@
 package com.university.StudentRegistration.model;
 
 import jakarta.persistence.Entity;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@Entity
+@Entity // tells spring the class is a database table
 public class GraduateStudent extends Student {
-
     private String researchThesisTitle;
+
+    // Spring Boot needs an empty constructor behind the scenes
+    public GraduateStudent() {
+    }
 
     public GraduateStudent(String name, String email, String major, String password, String thesisTitle) {
         super(name, email, major, password);
         this.researchThesisTitle = thesisTitle;
     }
 
-    // Polymorphism: Overriding the parent method
+    public String getResearchThesisTitle() {
+        return researchThesisTitle;
+    }
+
+    public void setResearchThesisTitle(String researchThesisTitle) {
+        this.researchThesisTitle = researchThesisTitle;
+    }
+
     @Override
     public String getProfileDisplayFormat() {
         return super.getProfileDisplayFormat() + " [Status: Graduate Researcher]";
--- src/main/java/com/university/StudentRegistration/model/UndergraduateStudent.java
+++ src/main/java/com/university/StudentRegistration/model/UndergraduateStudent.java
@@ -1,22 +1,28 @@
 package com.university.StudentRegistration.model;
 
 import jakarta.persistence.Entity;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@Entity
+@Entity // tells spring the class is a database table
 public class UndergraduateStudent extends Student {
-
     private String highSchoolName;
+
+    // Spring Boot needs an empty constructor behind the scenes
+    public UndergraduateStudent() {
+    }
 
     public UndergraduateStudent(String name, String email, String major, String password, String highSchoolName) {
         super(name, email, major, password);
         this.highSchoolName = highSchoolName;
     }
 
-    // Polymorphism: Overriding the parent method
+    public String getHighSchoolName() {
+        return highSchoolName;
+    }
+
+    public void setHighSchoolName(String highSchoolName) {
+        this.highSchoolName = highSchoolName;
+    }
+
     @Override
     public String getProfileDisplayFormat() {
         return super.getProfileDisplayFormat() + " [Status: Undergraduate]";
